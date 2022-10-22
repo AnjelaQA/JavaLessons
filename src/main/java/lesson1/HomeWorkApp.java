1753a5829aa172df58fa78e0a1497a1d1f558f54
AnjelaQA <anjela1996qa@gmail.com>
2022-10-22 04:22:06 +0300

    finished homework1

---
+++ src/main/java/lesson1/HomeWorkApp.java
@@ -0,0 +1,51 @@
+package lesson1;
+
+public class HomeWorkApp {
+    public static void main(String[] args) {
+        printThreeWords();
+        checkSumSign();
+        printColor();
+        compareNumbers();
+    }
+
+    private static void printThreeWords() {
+        System.out.println("Orange");
+        System.out.println("Banana");
+        System.out.println("Apple");
+
+    }
+
+    private static void checkSumSign() {
+        int a = 3;
+        int b = -3;
+        if (a + b >= 0) {
+            System.out.println("Сумма положительная");
+        } else {
+            System.out.println("Сумма отрицательная");
+        }
+        System.out.println();
+    }
+
+    private static void printColor() {
+        int value = 105;
+        if (value <= 0){
+            System.out.println("Красный");
+        } else if (value > 0 && value <= 100) {
+            System.out.println("Желтый");
+        } else {
+            System.out.println("Зеленый");
+        }
+        System.out.println();
+    }
+
+    private static void compareNumbers() {
+        int a = -3;
+        int b = 3;
+        if (a >= b) {
+            System.out.println("a >= b");
+        } else {
+            System.out.println("a < b");
+        }
+        System.out.println();
+    }
+}
